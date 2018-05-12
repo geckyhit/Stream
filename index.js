f0aaae183a907edbb85b053c506fdee9b54b855c
geckyhit <gekcy@mail.ru>
2018-05-12 14:05:08 +0300

    add hot stream

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const StreamVanil = require('./lib/StreamVanil')
 const StreamFunc = require('./lib/StreamFunc')
+const StreamVanilHot = require('./lib/StreamVanilHot')
 const { delay } = require('./lib/utils/async')
 
+//COLDS:
 const sv1 = new StreamVanil(async ({ emit }) => {
     emit(1)
     await delay(500)
     emit(2)
 })
 
-sv1.observe(x => console.log('sv1:', x))
+sv1.observe(x => console.log('sv1:', x)) //-1-2
+
 
 const sf1 = new StreamFunc(async (send) => {
     send(1)
@@ -16,4 +19,14 @@ const sf1 = new StreamFunc(async (send) => {
     send(2)
 })
 
-sf1.observe(x => console.log('sf1:', x))
+sf1.observe(x => console.log('sf1:', x)) //-1-2
+/*------*/
+
+//HOT:
+const svh1 = new StreamVanilHot(async ({ emit }) => {
+    emit(1)
+    await delay(500)
+    emit(2)
+})
+
+svh1.observe(x => console.log('svh1:', x))//-2

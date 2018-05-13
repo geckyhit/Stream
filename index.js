153bf905c2898eb8f32658e454ef86561ab80965
geckyhit <gekcy@mail.ru>
2018-05-13 04:11:10 +0300

    fix svh

--- index.js
+++ index.js
@@ -4,29 +4,36 @@ const StreamVanilHot = require('./lib/StreamVanilHot')
 const { delay } = require('./lib/utils/async')
 
 //COLDS:
-const sv1 = new StreamVanil(async ({ emit }) => {
-    emit(1)
-    await delay(500)
-    emit(2)
-})
+// const sv1 = new StreamVanil(async ({ emit }) => {
+//     emit(1)
+//     await delay(500)
+//     emit(2)
+// })
 
-sv1.observe(x => console.log('sv1:', x)) //-1-2
+// sv1.observe(x => console.log('sv1:', x)) //-1-2
 
 
-const sf1 = new StreamFunc(async (send) => {
-    send(1)
-    await delay(500)
-    send(2)
-})
+// const sf1 = new StreamFunc(async (send) => {
+//     send(1)
+//     await delay(500)
+//     send(2)
+// })
 
-sf1.observe(x => console.log('sf1:', x)) //-1-2
+// sf1.observe(x => console.log('sf1:', x)) //-1-2
 /*------*/
 
 //HOT:
 const svh1 = new StreamVanilHot(async ({ emit }) => {
+    await delay(50)
     emit(1)
-    await delay(500)
+    await delay(600)
     emit(2)
-})
+});
 
-svh1.observe(x => console.log('svh1:', x))//-2
+(async () => {
+    svh1.observe(x => console.log('svh1.0', x))
+    await delay(100)
+    svh1.observe(x => console.log('svh1.1', x))
+})()
+//svh1.0 @-1-2->
+//svh1.1   @-2->

00da2db2bc0d057418546b7d5bc5d8d430120950
geckyhit <gekcy@mail.ru>
2018-05-14 01:10:14 +0300

    add Push and Pull stream

--- index.js
+++ index.js
@@ -1,8 +1,47 @@
 const StreamVanil = require('./lib/StreamVanil')
 const StreamFunc = require('./lib/StreamFunc')
 const StreamVanilHot = require('./lib/StreamVanilHot')
+const PullStream = require('./lib/PullStream')
+const PushStream = require('./lib/PushStream')
 const { delay } = require('./lib/utils/async')
 
+/*********************************/
+// PullStream:
+let streamFn = (() => {
+    let countCb = 0;
+    return cb => {
+        cb(countCb)
+        countCb++
+    }
+})()
+
+let ps = new PullStream(streamFn)
+
+ps.consume(streamFn => {
+    streamFn(function next(countCb) {
+        console.log(countCb)
+        setTimeout(() => {
+            streamFn(next)
+        }, 1000)
+    })
+})
+/*********************************/
+
+
+
+/*********************************/
+//PushStream:
+let s = new PushStream(observeFn => {
+    let c = 0
+    setInterval(() => {
+        observeFn(c++)
+    }, 1000)
+})
+
+s.observe(console.log)
+/*********************************/
+
+/*********************************/
 //COLDS:
 // const sv1 = new StreamVanil(async ({ emit }) => {
 //     emit(1)
@@ -20,20 +59,22 @@ const { delay } = require('./lib/utils/async')
 // })
 
 // sf1.observe(x => console.log('sf1:', x)) //-1-2
-/*------*/
+/*********************************/
 
+/*********************************/
 //HOT:
-const svh1 = new StreamVanilHot(async ({ emit }) => {
-    await delay(50)
-    emit(1)
-    await delay(600)
-    emit(2)
-});
+// const svh1 = new StreamVanilHot(async ({ emit }) => {
+//     await delay(50)
+//     emit(1)
+//     await delay(600)
+//     emit(2)
+// });
 
-(async () => {
-    svh1.observe(x => console.log('svh1.0', x))
-    await delay(100)
-    svh1.observe(x => console.log('svh1.1', x))
-})()
+// (async () => {
+//     svh1.observe(x => console.log('svh1.0', x))
+//     await delay(100)
+//     svh1.observe(x => console.log('svh1.1', x))
+// })()
 //svh1.0 @-1-2->
 //svh1.1   @-2->
+/*********************************/
